d38bffb865bb4004d6743d1e1e2e7cac6e885d65
NoLivesRemaining <mgdunne1@eircom.net>
2020-02-27 23:02:59 +0000

    Initial Commit

---
+++ Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SphereCollider))]
+public class FlockAgent : MonoBehaviour
+{
+    SphereCollider agentCollider;
+    public SphereCollider AgentCollider { get { return agentCollider; } }
+
+    void Start()
+    {
+        agentCollider = GetComponent<SphereCollider>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        transform.up = velocity;
+        transform.position += velocity * Time.deltaTime;
+    }
+
+}
